cc45a6442ca319a89b241ba5e13f319a56517871
Yinjia-Chen <108180795+Yinjia-Chen@users.noreply.github.com>
2024-09-26 19:35:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var differenceOfSum = function (nums) {
-    var elementSum = 0, digitSum = 0;   // 声明元素和变量elementSum 和 数字和变量digitSum
-    for (var i = 0; i < nums.length; i++) {
-        elementSum += nums[i];
-        while (nums[i] > 0) {
-            digitSum += nums[i] % 10;
-            nums[i] = Math.floor(nums[i] / 10);
+var differenceOfSum = function(nums) {
+    let elementSum = 0, digitSum = 0;  // 声明 元素和变量elementSum 和 数字和digitSum
+    for (let num of nums) {  // 循环遍历数组元素
+        elementSum += num;  // 计算元素和
+        while (num > 0) {
+            digitSum += num % 10;  // 每次循环取出num的最后一位数字，并累加到数字和digitSum
+            num = Math.floor(num / 10);  // 去掉num最后一位，并四舍五入下取整后重新赋值进入循环
         }
     }
-    return elementSum - digitSum;
+    return elementSum - digitSum;  // 返回绝对值差（元素和一定大于等于数字和）
 };
