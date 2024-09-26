2ac451839d905e7a21e67e4dc44702a985b70298
Yinjia-Chen <108180795+Yinjia-Chen@users.noreply.github.com>
2024-09-26 18:57:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var differenceOfSum = function (nums) {};
+var differenceOfSum = function (nums) {
+    var elementSum = 0, digitSum = 0;
+    for (var i = 0; i < nums.length; i++) {
+        elementSum += nums[i];
+        while (nums[i] > 0) {
+            digitSum += nums[i] % 10;
+            nums[i] = Math.floor(nums[i] / 10);
+        }
+    }
+    return elementSum - digitSum;
+};
