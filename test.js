969ae9f3f83ee2fea52d444676ae6f5935305daf
Yinjia-Chen <108180795+Yinjia-Chen@users.noreply.github.com>
2024-09-26 19:05:35 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var differenceOfSum = function (nums) {
-    var elementSum = 0, digitSum = 0;
+    var elementSum = 0, digitSum = 0;   // 声明元素和变量elementSum 和 数字和变量digitSum
     for (var i = 0; i < nums.length; i++) {
         elementSum += nums[i];
         while (nums[i] > 0) {
